6f1b0b26ec9ed2e29d5dcfd3409242e7572b149b
dima-zharyy <zharyydimka@gmail.com>
2022-05-20 22:09:04 +0300

    added-task-01-gallery-simplelightbox-via-npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+// ----------------------- Render gallery items -----------------------
+
+const galleryBox = document.querySelector('.gallery');
+
+function createGalleryMarkup(data) {
+  return data
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join('');
+}
+
+function renderGalleryElements(data) {
+  const galleryMarkup = createGalleryMarkup(data);
+  galleryBox.innerHTML = galleryMarkup;
+}
+
+renderGalleryElements(galleryItems);
+
+// ----------------------- SimpleLightbox initialization -----------------------
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
