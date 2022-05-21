f87aaf08ac26b6bab7b0890997760c7e284bf4b4
dima-zharyy <zharyydimka@gmail.com>
2022-05-21 12:19:15 +0300

    added-task-03-form-input-save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-msg';
+const inputData = {};
+
+populateUserInput();
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onInput(event) {
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+}
+
+function populateUserInput() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!savedData) return;
+
+  if (savedData.email) {
+    form.elements.email.value = savedData.email;
+  }
+
+  if (savedData.message) {
+    form.elements.message.value = savedData.message;
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  if (!event.currentTarget.elements.email.value || !event.currentTarget.elements.message.value) {
+    alert('All fields must be filled!');
+    return;
+  }
+
+  const submittedData = new FormData(event.currentTarget);
+  const sendOnServerData = {};
+  submittedData.forEach((value, name) => (sendOnServerData[name] = value));
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(sendOnServerData);
+}
