2d5216294457fb49ba491670ac57c7db936ffeab
dima-zharyy <zharyydimka@gmail.com>
2022-05-21 10:57:21 +0300

    added-task-02-vimeo-player-save-current-time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
+
+populateLastVideoTime();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  const currentTimeSeconds = event.seconds;
+  localStorage.setItem(KEY_STORAGE, currentTimeSeconds);
+}
+
+function populateLastVideoTime() {
+  const savedTime = localStorage.getItem(KEY_STORAGE);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
